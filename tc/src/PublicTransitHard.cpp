e09b5dc7f90fc9c78e345240c5922478a58e257e
Caiwai Fung <gdfqw93@163.com>
2015-05-21 15:46:43 +0800

    .

--- tc/src/PublicTransitHard.cpp
+++ tc/src/PublicTransitHard.cpp
@@ -1,3 +1,4 @@
+// 14:27 - 15:15 - wa sample
 #include <algorithm>
 #include <iostream>
 #include <sstream>
@@ -37,10 +38,136 @@ template<class T> T gcd(T a, T b) { return b==0?a:gcd(b,a%b); }
 
 // ------------------------ template ends here ------------------------ //
 
-class PublicTransitHard {
+const int MAXN=2020;
+
+class Cycle {
+    VPI tr[MAXN*2];
+    int X;
+
+    void init() {
+        forn(i, MAXN*2) {
+            tr[i].clear();
+            tr[i].PB(MP(-1, 1<<28));
+        }
+    }
+    void add(int i, int x, int y) { // i in [-N,N]
+        i+=2001;
+        for(; i<MAXN*2; i+=i&-i)
+            if(tr[i].back().se>y)
+                tr[i].PB(MP(x,y));
+    }
+    void del(int i, int x) {
+        i+=2001;
+        for(; i<MAXN*2; i+=i&-i)
+            if(tr[i].back().fi==x)
+                tr[i].pop_back();
+    }
+    int get(int i) {
+        i+=2001;
+        i=min(MAXN*2-1, max(i,0));
+        int ans=1<<28;
+        for(; i>=1; i-=i&-i)
+            setmin(ans, tr[i].back().se);
+        return ans;
+    }
+
+    int a[MAXN], lim[MAXN];
+    int b[MAXN], mxb[MAXN];
+    int i;
+
 public:
-    int countValidTeleporters(int N, vector <int> edges, int X) {
-        return int();
+    Cycle(int X) : X(X) {
+        init();
+        i=0;
+        lim[0]=1<<28, mxb[0]=-1;
+    }
+    void push(int ca, int cb) {
+        ++i;
+        b[i]=cb, mxb[i]=max(mxb[i-1], cb);
+        a[i]=ca;
+
+        int tmp=get(a[i]+i-X-1);
+        lim[i]=min(lim[i-1], tmp-a[i]+i);
+
+        add(i-a[i], i, X-a[i]-i+1);
+    }
+    void pop() {
+        del(i-a[i], i);
+        --i;
+    }
+    bool ok() {
+        /*
+        forint(k, 1, i) if(b[k]>X) return false;
+        forint(x, 1, i) forint(y, x+1, i) if(a[x]+a[y]+y-x>X && a[x]+a[y]+i-1-(y-x)>X) return false;
+        return true;
+        */
+        return i<=lim[i] && mxb[i]<=X;
+    }
+};
+
+class PublicTransitHard {
+    VI g[MAXN];
+    Cycle *c;
+    int ans, root;
+
+    int len[MAXN], diameter[MAXN];
+
+    void prepare(int x, int fa) {
+        len[x]=diameter[x]=0;
+        for(int y: g[x]) if(y!=fa) {
+            prepare(y, x);
+            setmax(diameter[x], diameter[y]);
+            setmax(diameter[x], len[x]+1+len[y]);
+            setmax(len[x], len[y]+1);
+        }
+    }
+    void trim(VI &v, int n) {
+        sort(all(v), greater<int>());
+        v.resize(n, -1000);
+    }
+    void dfs(int x, int fa) {
+        if(x<=root) {
+            c->push(len[x], diameter[x]);
+            if(c->ok()) ++ans;
+            c->pop();
+        }
+
+        VI ls, ds;
+        for(int y: g[x]) if(y!=fa) {
+            ls.PB(len[y]), ds.PB(diameter[y]);
+        }
+        trim(ls, 3);
+        trim(ds, 2);
+        for(int y: g[x]) if(y!=fa) {
+            int li=0, lj=1, di=0;
+            if(ls[li]==len[y]) ++li, ++lj;
+            else if(ls[lj]==len[y]) ++lj;
+            if(ds[di]==diameter[y]) ++di;
+
+            int cl=max(0, ls[li]+1);
+            int cd=max(0, max(ds[di], ls[li]+ls[lj]+2));
+            c->push(cl, cd);
+            dfs(y, x);
+            c->pop();
+        }
+    }
+public:
+    int countValidTeleporters(int n, vector <int> edges, int X) {
+        forint(i, 1, n) g[i].clear();
+        forn(i, edges.size()) {
+            int a=i+2, b=edges[i]+1;
+            g[a].PB(b), g[b].PB(a);
+        }
+
+        ans=0;
+        c=new Cycle(X);
+        forint(i, 1, n) {
+            root=i;
+            prepare(i, 0);
+            dfs(i, 0);
+        }
+        
+        return int(ans);
     }
     
 // BEGIN CUT HERE
@@ -64,7 +191,7 @@ public:
 int main(){
     PublicTransitHard ___test;
     /*
-       ___test.run_test(0);
+       ___test.run_test(5);
     /*/
       ___test.run_test(-1);
     //*/
