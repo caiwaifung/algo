74910376661f0356820b5d2f48a9f66ff588fb43
Caiwai Fung <gdfqw93@163.com>
2015-06-14 18:29:23 +0800

    .

--- gcj/2015/r3/d/d.cpp
+++ gcj/2015/r3/d/d.cpp
@@ -37,79 +37,112 @@ typedef vector<LL> VL;
 typedef vector<int> VI;
 typedef vector<PII> VPI;
 typedef vector<string> VS;
+typedef map<LL, LL> MLL;
 template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } return false; }
 template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
 template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
 
-map<LL, LL> a;
-int n;
+MLL a;
 
 void init() {
     int m; cin>>m;
-    static LL p[55555], f[55555];
+    static LL p[10010], f[10010];
     a.clear();
     forint(i, 1, m) cin>>p[i];
     forint(i, 1, m) cin>>f[i];
-    LL sum=0;
-    forint(i, 1, m) {
-        a[p[i]]+=f[i];
-        sum+=f[i];
-    }
-    n=0;
-    while((1LL<<n)<sum) ++n;
-    assert((1LL<<n)==sum);
+    forint(i, 1, m) a[p[i]]+=f[i];
 }
 
-LL ans[66];
+VL ans;
 
-bool reduce(map<LL, LL> &s, LL d) {
-    vector<PLL> vs(all(s));
-    if(d==0) {
-        s.clear();
-        for(PLL p: vs) {
-            if(p.se%2!=0) return false;
-            s[p.fi]=p.se/2;
+void extract_zeros() {
+    LL n1=a.begin()->se;
+    LL n2=a.rbegin()->se;
+    assert(n1==n2);
+    LL n=max(n1, n2);
+    //_debug("n=%lld\n",n);
+    auto tmp=a;
+    a.clear();
+    for(PLL p: tmp) {
+        assert(p.se%n==0);
+        a[p.fi]=p.se/n;
+    }
+    int num=0;
+    while((1LL<<num)<n) {
+        ++num;
+        ans.PB(0);
+    }
+}
+
+MLL extract(const MLL &t, LL x) {
+    assert(x>=0);
+    MLL u=t, r;
+    for(PLL p: t) {
+        LL num=u[p.fi];
+        if(num>0) {
+            assert(u[p.fi+x]>=num);
+            u[p.fi]-=num;
+            u[p.fi+x]-=num;
+            r[p.fi]=num;
         }
-        return true;
     }
-    LL tmp=0;
-    if(d<0) { d=-d; tmp=d; }
-    for(PLL p: vs) {
-        LL num=s[p.fi];
-        if(s[p.fi+d]<num) return false;
-        s[p.fi+d]-=num;
+    return r;
+}
+
+bool able(VL xs, LL r) {
+    VL us; us.PB(0);
+    for(LL x: xs) {
+        VL vs;
+        for(LL u: us) {
+            vs.PB(u);
+            vs.PB(u+x);
+        }
+        sort(all(vs));
+        vs.erase(unique(all(vs)), vs.end());
+        us=vs;
     }
-    map<LL, LL> t;
-    for(PLL p: s) if(p.se!=0)
-        t[p.fi+tmp]=p.se;
-    s=t;
-    return true;
+    return find(all(us), r)!=us.end();
 }
 
 void solve(int cs) {
-    assert(a[0]>0);
+    ans.clear();
+    extract_zeros();
 
-    forint(i, 1, n) {
-        bool found=false;
-        for(PLL p: a) if(p.se>0) {
-            map<LL, LL> b=a;
-            if(reduce(b, p.fi)) {
-                found=true;
-                ans[i]=p.fi;
-                a=b;
-                break;
-            }
-        }
-        assert(found);
+    VL xs;
+    while(a.size()>1) {
+        assert(a.begin()->se==1);
+        LL x=(++a.begin())->fi-a.begin()->fi;
+        xs.PB(x);
+        a=extract(a, x);
     }
+    assert(a.size()==1 && a.begin()->se==1);
+    LL r=-a.begin()->fi;
+    assert(r>=0);
 
-    printf("Case #%d: ", cs); fflush(stdout);
-    forint(i, 1, n) cout<<ans[i]<<" "; cout<<endl;
+    sort(all(xs));
+    while(xs.size()>0) {
+        LL x=xs.back();
+        xs.pop_back();
+        if(able(xs, r-x)) {
+            ans.PB(-x);
+            r-=x;
+        }
+        else {
+            assert(able(xs, r));
+            ans.PB(x);
+        }
+    }
+    assert(r==0);
+
+    sort(all(ans));
+    cout<<"Case #"<<cs<<": ";
+    for(LL x: ans) cout<<x<<" "; cout<<endl;
 }
 
 int main() {
     int csn; scanf("%d", &csn);
     forint(cs, 1, csn) {
+        _debug("cs=%d/%d\n",cs,csn);
         init();
         solve(cs);
     }
