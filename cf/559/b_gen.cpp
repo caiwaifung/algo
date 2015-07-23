74d87456963779d808b9b50a1e2847779924d7b1
Caiwai Fung <gdfqw93@163.com>
2015-07-23 16:39:23 +0800

    .

---
+++ cf/559/b_gen.cpp
@@ -0,0 +1,54 @@
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+#define rep(i, a, b) for(int i=int(a); i<=int(b); ++i)
+#define irep(i, a, b) for(int i=int(a); i>=int(b); --i)
+#define repn(i, n) rep(i, 0, (n)-1)
+#define irepn(i, n) irep(i, (n)-1, 0)
+#define fillchar(a, x) memset(a, x, sizeof(a))
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#ifdef DEBUG
+    #define _debug(args...) { fprintf(stderr, args); fflush(stderr); }
+#else
+    #define _debug(args...) {}
+#endif
+typedef long long LL;
+typedef pair<LL,LL> PLL;
+typedef pair<int,int> PII;
+typedef pair<double,double> PDD;
+typedef vector<LL> VL;
+typedef vector<int> VI;
+typedef vector<PII> VPI;
+typedef vector<string> VS;
+template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } return false; }
+template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
+template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
+
+char s[99999], t[99999];
+
+void go(char a[], int le, int ri) {
+    if(le+1==ri) return;
+    int mid=(le+ri)/2;
+    repn(i, mid-le) swap(a[le+i], a[mid+i]);
+}
+
+int main() {
+    repn(i, 65536) s[i]=char('a'+rand()%26);
+    memmove(t, s, sizeof(t));
+    go(t, 0, 65536);
+    s[65536]='\0';
+    t[65536]='\0';
+    printf("%s\n", s);
+    printf("%s\n", t);
+    return 0;
+}
