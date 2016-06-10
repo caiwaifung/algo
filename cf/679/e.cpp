ff57489f0cd0e5849d72b0f98476dd56670a916b
Qiwei Feng <caiwaifung@gmail.com>
2016-06-10 09:33:49 -0700

    .

---
+++ cf/679/e.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define sz(a) static_cast<int>(a.size())
+#define all(a) a.begin(), a.end()
+#define rep(i, a, b) for(int i=int(a); i<=int(b); ++i)
+#define irep(i, a, b) for(int i=int(a); i>=int(b); --i)
+#define repn(i, n) rep(i, 0, (n)-1)
+#define irepn(i, n) irep(i, (n)-1, 0)
+#define fillchar(a, x) memset(a, x, sizeof(a))
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+typedef long long LL;
+typedef pair<LL,LL> PLL;
+typedef pair<int,int> PII;
+typedef pair<double,double> PDD;
+typedef vector<LL> VL;
+typedef vector<int> VI;
+typedef vector<PII> VPI;
+typedef vector<string> VS;
+template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } return false; }
+template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
+template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
+
+// a[1..n], b[1..n]
+struct Abstract {
+public:
+    void init(int n, int initial[]) {
+    }
+
+    void add_a(int l, int r, LL v) {
+    }
+
+    void set(int l, int r, LL va, LL vb) {
+    }
+
+    // (a[i], b[i])
+    PLL query_ind(int i) {
+    }
+
+    // (min_i b[i]-a[i], i)
+    pair<LL,int> query_min_delta() {
+    }
+} abstract;
+
+int main() {
+    return 0;
+}
