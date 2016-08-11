1c5cdb759b009522c24ea6b68465fecaa0a690c0
Qiwei Feng <caiwaifung@gmail.com>
2016-08-11 23:48:35 +1000

    improve base template

--- template/0-cc/base.cc
+++ template/0-cc/base.cc
@@ -12,13 +12,15 @@
 #include <vector>
 using namespace std;
 
-#define sz(a) static_cast<int>(a.size())
-#define all(a) a.begin(), a.end()
+#define all(a) (a).begin(), (a).end()
+#define sz(a) static_cast<int>((a).size())
+#define fillchar(a, x) memset(a, x, sizeof(a))
 #define rep(i, a, b) for(int i=int(a); i<=int(b); ++i)
 #define irep(i, a, b) for(int i=int(a); i>=int(b); --i)
+#define replr(i, a, b) rep(i, a, (b)-1)
+#define reprl(i, a, b) irep(i, (b)-1, a)
 #define repn(i, n) rep(i, 0, (n)-1)
 #define irepn(i, n) irep(i, (n)-1, 0)
-#define fillchar(a, x) memset(a, x, sizeof(a))
 #define fi first
 #define se second
 #define pb push_back
@@ -31,6 +33,8 @@ typedef vector<LL> VL;
 typedef vector<int> VI;
 typedef vector<PII> VPI;
 typedef vector<string> VS;
+template<class T, class S> ostream& operator<<(ostream& os, const pair<T, S>& v) { return os<<"("<<v.first<<", "<<v.second<<")"; }
+template<class T> ostream& operator<<(ostream& os, const vector<T>& v) { os<<"["; repn(i, sz(v)) { if(i) os<<", "; os<<v[i]; } return os<<"]"; }
 template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } return false; }
 template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
 template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
--- template/0-cc/topcoder.cc
+++ template/0-cc/topcoder.cc
@@ -1,21 +1,26 @@
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <sstream>
+#include <limits>
+#include <memory>
+#include <queue>
 #include <string>
 #include <vector>
 using namespace std;
 
-#define _ (int)
-#define all(a) a.begin(), a.end()
+#define all(a) (a).begin(), (a).end()
+#define sz(a) static_cast<int>((a).size())
+#define fillchar(a, x) memset(a, x, sizeof(a))
 #define rep(i, a, b) for(int i=int(a); i<=int(b); ++i)
 #define irep(i, a, b) for(int i=int(a); i>=int(b); --i)
+#define replr(i, a, b) rep(i, a, (b)-1)
+#define reprl(i, a, b) irep(i, (b)-1, a)
 #define repn(i, n) rep(i, 0, (n)-1)
 #define irepn(i, n) irep(i, (n)-1, 0)
-#define fillchar(a, x) memset(a, x, sizeof(a))
 #define fi first
 #define se second
 #define pb push_back
@@ -28,6 +33,8 @@ typedef vector<LL> VL;
 typedef vector<int> VI;
 typedef vector<PII> VPI;
 typedef vector<string> VS;
+template<class T, class S> ostream& operator<<(ostream& os, const pair<T, S>& v) { return os<<"("<<v.first<<", "<<v.second<<")"; }
+template<class T> ostream& operator<<(ostream& os, const vector<T>& v) { os<<"["; repn(i, sz(v)) { if(i) os<<", "; os<<v[i]; } return os<<"]"; }
 template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } return false; }
 template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
 template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
