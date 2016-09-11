fd8224d248424099aa6c65fc04e532c3cd184eed
Qiwei Feng <caiwaifung@gmail.com>
2016-09-11 11:08:43 +1000

    review graph/KM

--- acm/ural/1076.cc
+++ acm/ural/1076.cc
@@ -40,7 +40,55 @@ template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } r
 template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
 template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
 
+const int N=150;
+
+int a[N][N], n;
+
+int mincost_match() {
+    VI x(n, -1000), y(n), matched(n, -1);
+    repn(cur, n) {
+        VI slack(n, 1<<30), pre(n, -1);
+        vector<bool> vis(n, false);
+        int j0=-1;
+        while(1) {
+            const int i0=(j0<0 ? cur : matched[j0]);
+            if(i0<0) break;
+            int minv=1<<30, minj=-1;
+            repn(j, n) if(!vis[j]) {
+                if(setmin(slack[j], a[i0][j]-x[i0]-y[j])) {
+                    pre[j]=j0;
+                }
+                if(setmin(minv, slack[j])) {
+                    minj=j;
+                }
+            }
+            x[cur]+=minv;
+            repn(j, n) if(vis[j]) {
+                x[matched[j]]+=minv;
+                y[j]-=minv;
+            } else {
+                slack[j]-=minv;
+            }
+            j0=minj, vis[j0]=true;
+        }
+        while(j0>=0) {
+            const int j=pre[j0];
+            matched[j0]=(j<0 ? cur : matched[j]), j0=j;
+        }
+    }
+    return accumulate(all(x), 0)+accumulate(all(y), 0);
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
+    cin>>n;
+    int sum=0;
+    repn(i, n) repn(j, n) {
+        int x; cin>>x;
+        sum+=x;
+        a[i][j]=-x;
+    }
+    sum+=mincost_match();
+    cout<<sum<<endl;
     return 0;
 }
