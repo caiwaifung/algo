d8e90aa37cbd48e5a54d4561ca1ec7422304bdf1
Qiwei Feng <caiwaifung@gmail.com>
2016-09-18 15:09:26 +1000

    review kmp+manacher for CF30E

---
+++ template/string/kmp.cc
@@ -0,0 +1,16 @@
+VI kmp(const string& s, const string& t) {
+    VI fail(sz(t)+1);
+    int j=fail[0]=-1;
+    repn(i, sz(t)) {
+        while(j>=0 && t[j]!=t[i]) j=fail[j];
+        fail[i+1]=++j;
+    }
+
+    VI ans(sz(s));
+    j=0;
+    repn(i, sz(s)) {
+        while(j>=0 && t[j]!=s[i]) j=fail[j];
+        ans[i]=++j;
+    }
+    return ans;
+}
