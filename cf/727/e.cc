7489ac19f42b05ae6d44cada7a1a32c59066c51b
Qiwei Feng <caiwaifung@gmail.com>
2016-10-16 00:50:51 +1100

    .

--- cf/727/e.cc
+++ cf/727/e.cc
@@ -29,6 +29,7 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 typedef long long LL;
+typedef unsigned long long ULL;
 typedef pair<LL,LL> PLL;
 typedef pair<int,int> PII;
 typedef pair<double,double> PDD;
@@ -56,6 +57,7 @@ VS read_VS() {
 }
 
 const LL P=911;
+const LL P2=127;
 const LL M=LL(1e9)+7;
 
 int main() {
@@ -63,18 +65,26 @@ int main() {
     string s=read_str(); s=s+s;
     VS games=read_VS();
 
-    map<LL, int> m;
+    map<pair<LL,ULL>, int> m;
     repn(i, sz(games)) {
-        LL h=0;
-        for(char c: games[i]) h=(h*P+c)%M;
-        m[h]=i;
+        LL h=0; ULL h2=0;
+        for(char c: games[i]) {
+            h=(h*P+c)%M;
+            h2=h2*P2+c;
+        }
+        m[mp(h,h2)]=i;
     }
     LL h=0, pk=1;
-    repn(i, k) { h=(h*P+s[i])%M; pk=(pk*P)%M; }
+    ULL h2=0, pk2=1;
+    repn(i, k) {
+        h=(h*P+s[i])%M; pk=(pk*P)%M;
+        h2=h2*P2+s[i]; pk2=pk2*P2;
+    }
     VI match(n*k, -1);
     repn(i, n*k) {
-        if(m.count(h)) match[i]=m[h];
+        if(m.count(mp(h,h2))) match[i]=m[mp(h,h2)];
         h=((h*P+s[i+k]-LL(s[i])*pk)%M+M)%M;
+        h2=h2*P2+s[i+k]-ULL(s[i])*pk2;
     }
 
     repn(i, k) {
--- cf/727/f.cc
+++ cf/727/f.cc
@@ -40,105 +40,24 @@ template<class T> bool setmax(T &_a, T _b) { if(_b>_a) { _a=_b; return true; } r
 template<class T> bool setmin(T &_a, T _b) { if(_b<_a) { _a=_b; return true; } return false; }
 template<class T> T gcd(T _a, T _b) { return _b==0?_a:gcd(_b,_a%_b); }
 
-class IntervalTree {
-    struct Node {
-        pair<LL, int> minp;
-        LL buf;
-    };
-    const int n_;
-    vector<Node> tr_;
-
-    void release(int x) {
-        if(tr_[x].buf!=0) {
-            tr_[x*2+1].minp.fi+=tr_[x].buf;
-            tr_[x*2+1].buf+=tr_[x].buf;
-            tr_[x*2+2].minp.fi+=tr_[x].buf;
-            tr_[x*2+2].buf+=tr_[x].buf;
-            tr_[x].buf=0;
-        }
-    }
-
-    void update(int x) {
-        tr_[x].minp=min(tr_[x*2+1].minp, tr_[x*2+2].minp);
-    }
-
-    void build(int x, int l, int r) {
-        tr_[x].minp=mp(0, l);
-        tr_[x].buf=0;
-        if(l+1==r) return;
-        int m=(l+r)/2;
-        build(x*2+1, l, m);
-        build(x*2+2, m, r);
-    }
-    pair<LL, int> minval(int x, int l, int r, int s, int t) {
-        if(s<=l && r<=t) {
-            return tr_[x].minp;
-        }
-        release(x);
-        const int m=(l+r)/2;
-        pair<LL, int> ans{1LL<<60, -1};
-        if(s<m) setmin(ans, minval(x*2+1, l, m, s, t));
-        if(m<t) setmin(ans, minval(x*2+2, m, r, s, t));
-        return ans;
-    }
-    void add(int x, int l, int r, int s, int t, LL val) {
-        if(s<=l && r<=t) {
-            tr_[x].minp.fi+=val;
-            tr_[x].buf+=val;
-            return;
-        }
-        release(x);
-        const int m=(l+r)/2;
-        if(s<m) add(x*2+1, l, m, s, t, val);
-        if(m<t) add(x*2+2, m, r, s, t, val);
-        update(x);
-    }
-public:
-    IntervalTree(int n) : n_(n), tr_(n*4) {
-        build(0, 0, n_);
-    }
-    pair<LL, int> minval(int s, int t) {
-        return minval(0, 0, n_, s, t);
-    }
-    void add(int s, int t, LL val) {
-        add(0, 0, n_, s, t, val);
-    }
-};
-
 int main() {
     int n, m; scanf("%d%d", &n,&m);
-    IntervalTree ta(n), ts(n);
-    LL sum=0;
-    repn(i, n) {
-        int x; scanf("%d", &x);
-        sum+=x;
-        ta.add(i, i+1, x);
-        ts.add(i, i+1, sum);
-    }
-    vector<pair<LL, int>> queries(m);
-    repn(i, m) {
-        scanf("%lld", &queries[i].fi);
-        queries[i].se=i;
-    }
-    sort(all(queries), greater<pair<LL, int>>());
+    VI a(n); repn(i, n) scanf("%d", &a[i]);
 
-    VI ans(m); int cur=0;
-    for(const auto& p: queries) {
-        //cout<<endl<<"query: "<<p<<endl;
-        while(1) {
-            const auto minp=ts.minval(0, n);
-            //cout<<"minp: "<<minp<<endl;
-            if(p.fi+minp.fi>=0) break;
-            const auto z=ta.minval(0, minp.se+1);
-            //cout<<"z: "<<z<<endl;
-            ta.add(z.se, z.se+1, -z.fi);
-            ts.add(z.se, n, -z.fi);
-            ++cur;
+    vector<VL> f(n+1, VL(n+1));
+    fill(all(f[n]), 0);
+    irepn(i, n) {
+        rep(c, 0, n) {
+            f[i][c]=min((LL)a[i], f[i+1][c]+a[i]);
+            if(c>0) setmax(f[i][c], f[i+1][c-1]);
         }
-        ans[p.se]=cur;
+    //cout<<"f["<<i<<"]: "<<f[i]<<endl;
     }
-    repn(i, m) printf("%d\n", ans[i]);
-
+    while(m--) {
+        LL b; scanf("%lld", &b);
+        int p=int(lower_bound(all(f[0]), -b)-f[0].begin());
+        printf("%d\n", p);
+    }
 
     return 0;
 }
