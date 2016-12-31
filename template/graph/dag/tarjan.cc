bafd6686506c29ee7a11cc5e95e63d964ce849a2
Qiwei Feng <caiwaifung@gmail.com>
2016-12-31 17:09:42 +1100

    .

--- template/graph/dag/tarjan.cc
+++ template/graph/dag/tarjan.cc
@@ -4,29 +4,30 @@ int n;
 void solve() {
     VI belong(n, -1), dfn(n), low(n);
     VI stack;
-    int num=0, counter=0;
-    const function<void(int)> dfs=[&](int x) {
-        dfn[x]=low[x]=counter++;
-        belong[i]=-2;
+    int num = 0, counter = 0;
+    const function<void(int)> dfs = [&](int x) {
+        dfn[x] = low[x] = counter++;
+        belong[i] = -2;
         stack.pb(x);
-        for(int y: es[x]) {
-            if(belong[y]==-1) {
+        for(int y : es[x]) {
+            if(belong[y] == -1) {
                 dfs(y);
                 setmin(low[x], low[y]);
-            } else if(belong[y]==-2) {
+            } else if(belong[y] == -2) {
                 setmin(low[x], dfn[y]);
             }
         }
-        if(low[x]==dfn[x]) {
+        if(low[x] == dfn[x]) {
             while(1) {
-                const int y=stack.back(); stack.pop_back();
-                belong[y]=num;
-                if(y==x) break;
+                const int y = stack.back();
+                stack.pop_back();
+                belong[y] = num;
+                if(y == x) break;
             }
             ++num;
         }
     };
-    repn(i, n) if(belong[i]==0) dfs(i);
+    repn(i, n) if(belong[i] == 0) dfs(i);
 
     // ...
 }
