6d4af17c7f779a43bf82ec30c41d97a46cf4dc74
Qiwei Feng <caiwaifung@gmail.com>
2017-01-07 16:55:37 +1100

    .

--- template/base/main-cases.h
+++ template/base/main-cases.h
@@ -1,11 +1,10 @@
 void solve(int cs) {
-
     printf("Case #%d: ", cs);
 }
 
 int main() {
     int num;
     scanf("%d", &num);
-    rep(i, 1, num) { solve(i + 1); }
+    rep(i, 1, num) { solve(i); }
     return 0;
 }
