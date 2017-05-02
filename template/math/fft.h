4b7ac5164c7c84fd956193c4405c95f3c1d4f989
Qiwei Feng <caiwaifung@gmail.com>
2017-05-02 12:21:10 -0700

    extend fft to fields

--- template/math/fft.h
+++ template/math/fft.h
@@ -1,26 +1,24 @@
-typedef complex<double> Complex;
-void fft(vector<Complex>* ptr_a, bool is_inv = false) {
-    auto& a = *ptr_a;
-    int n = sz(a);
-    static vector<Complex> b;
+template <class Field, class RandomIt>
+void fft(RandomIt a, RandomIt a_end, bool is_inv = false) {
+    typedef typename Field::value_t value_t;
+    const int n = a_end - a;
+    static vector<value_t> b;
     if(sz(b) < n) b.resize(n);
 
-    double arg = acos(-1.0);
     for(int t = n / 2; t >= 1; t /= 2) {
-        Complex w(cos(arg), is_inv ? sin(arg) : -sin(arg));
-        Complex p(1.0, 0.0);
-        for(int j = 0; j < n / 2; j += t, p *= w) {
+        value_t w = Field::root_of_unity(n / t, is_inv);
+        value_t p = 1;
+        for(int j = 0; j < n / 2; j += t, p = Field::mul(p, w)) {
             for(int i = 0; i < t; ++i) {
-                Complex u = a[i + j * 2];
-                Complex v = p * a[i + t + j * 2];
-                b[i + j] = u + v;
-                b[i + j + n / 2] = u - v;
+                value_t u = a[i + j * 2];
+                value_t v = Field::mul(p, a[i + t + j * 2]);
+                b[i + j] = Field::add(u, v);
+                b[i + j + n / 2] = Field::sub(u, v);
             }
         }
-        copy(b.begin(), b.begin() + n, a.begin());
-        arg /= 2;
+        copy(b.begin(), b.begin() + n, a);
     }
     if(is_inv) {
-        repn(i, n) a[i] /= n;
+        repn(i, n) a[i] = Field::div(a[i], n);
     }
 }
--- template/math/tests/fft_test.cc
+++ template/math/tests/fft_test.cc
@@ -2,7 +2,10 @@
 #include <gtest/gtest.h>
 
 #include "../../base/header.h"
+
 #include "../fft.h"
+#include "../field-complex.h"
+#include "../field-intmod.h"
 
 #define IS_COMPLEX(r, i)                                               \
     testing::AllOf(                                                    \
@@ -11,21 +14,38 @@
 #define IS_REAL(r) IS_COMPLEX(r, 0)
 
 TEST(FFTComplex, Convolution_4) {
-    vector<Complex> a = {1, 1, 0, 0}, b = {2, 3, 0, 0};
-    fft(&a), fft(&b);
+    vector<complex<double>> a = {1, 1, 0, 0}, b = {2, 3, 0, 0};
+    fft<ComplexField>(all(a)), fft<ComplexField>(all(b));
     repn(i, sz(a)) a[i] *= b[i];
-    fft(&a, true);
+    fft<ComplexField>(all(a), true);
     EXPECT_THAT(a, testing::ElementsAre(IS_REAL(2), IS_REAL(5), IS_REAL(3),
                                         IS_REAL(0)));
 }
 
 TEST(FFTComplex, Convolution_8) {
-    vector<Complex> a = {1, 2, 3, 4, 0, 0, 0, 0};
-    vector<Complex> b = {5, 4, 3, 2, 1, 0, 0, 0};
-    fft(&a), fft(&b);
+    vector<complex<double>> a = {1, 2, 3, 4, 0, 0, 0, 0};
+    vector<complex<double>> b = {5, 4, 3, 2, 1, 0, 0, 0};
+    fft<ComplexField>(all(a)), fft<ComplexField>(all(b));
     repn(i, sz(a)) a[i] *= b[i];
-    fft(&a, true);
+    fft<ComplexField>(all(a), true);
     EXPECT_THAT(a, testing::ElementsAre(IS_REAL(5), IS_REAL(14), IS_REAL(26),
                                         IS_REAL(40), IS_REAL(30), IS_REAL(20),
                                         IS_REAL(11), IS_REAL(4)));
+}
+
+TEST(FFTInt, Convolution_4) {
+    VI a = {1, 1, 0, 0}, b = {2, 3, 0, 0};
+    fft<IntModField>(all(a)), fft<IntModField>(all(b));
+    repn(i, sz(a)) a[i] = LL(a[i]) * b[i] % MOD;
+    fft<IntModField>(all(a), true);
+    EXPECT_THAT(a, testing::ElementsAre(2, 5, 3, 0));
+}
+
+TEST(FFTInt, Convolution_8) {
+    VI a = {1, 2, 3, 4, 0, 0, 0, 0};
+    VI b = {5, 4, 3, 2, 1, 0, 0, 0};
+    fft<IntModField>(all(a)), fft<IntModField>(all(b));
+    repn(i, sz(a)) a[i] = LL(a[i]) * b[i] % MOD;
+    fft<IntModField>(all(a), true);
+    EXPECT_THAT(a, testing::ElementsAre(5, 14, 26, 40, 30, 20, 11, 4));
 }
