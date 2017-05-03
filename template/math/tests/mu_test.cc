a20b849ce292196b4400b51a6863a5bd3c7c0901
Qiwei Feng <caiwaifung@gmail.com>
2017-05-02 21:00:50 -0700

    add mobius inversion test

--- template/math/tests/mu_test.cc
+++ template/math/tests/mu_test.cc
@@ -1,5 +1,6 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <random>
 
 #include "../../base/header.h"
 #include "../mu.h"
@@ -34,4 +35,22 @@ TEST(Mu, At200000) {
     auto r = compute_mu(200000);
     EXPECT_THAT(r, SizeIs(200001));
     repn(i, 20) EXPECT_EQ(mu(200000 - i), r[200000 - i]);
+}
+
+TEST(Mu, MobiusInversion) {
+    // g(n) = sum_{d\n} f(d) <=>
+    // f(n) = sum_{d\n} g(d) mu(n/d)
+    const int N = 1000;
+    VI f(N + 1), g(N + 1);
+    std::uniform_real_distribution<int> distr(-1000, 1000);
+    std::default_random_engine gen;
+    rep(n, 1, N) f[n] = distr(gen);
+    rep(n, 1, N) rep(d, 1, n) if(n % d == 0) g[n] += f[d];
+
+    auto m = compute_mu(N);
+    rep(n, 1, N) {
+        int res = 0;
+        rep(d, 1, n) if(n % d == 0) res += g[d] * m[n / d];
+        EXPECT_EQ(res, f[n]);
+    }
 }
