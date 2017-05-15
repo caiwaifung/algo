a9726becfba8de9fa9c81ecf9f81271447c50132
Qiwei Feng <caiwaifung@gmail.com>
2017-05-15 12:29:37 -0700

    .

--- template/base/header.h
+++ template/base/header.h
@@ -47,11 +47,21 @@ typedef vector<double> VD;
 typedef vector<string> VS;
 typedef vector<PII> VPI;
 typedef vector<PLL> VPL;
+typedef vector<VI> VVI;
+typedef vector<VL> VVL;
+typedef vector<VD> VVD;
+typedef vector<VS> VVS;
+typedef vector<VPI> VVPI;
+typedef vector<VPL> VVPL;
 template <class T, class S> ostream& operator<<(ostream& os, const pair<T, S>& v) { return os << "(" << v.first << ", " << v.second << ")"; }
 template <class T> ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; repn(i, sz(v)) { if(i) os << ", "; os << v[i]; } return os << "]"; }
 template <class T> bool setmax(T& _a, T _b) { if(_a < _b) { _a = _b; return true; } return false; }
 template <class T> bool setmin(T& _a, T _b) { if(_b < _a) { _a = _b; return true; } return false; }
 template <class T> T gcd(T _a, T _b) { return _b == 0 ? _a : gcd(_b, _a % _b); }
+VI read_vi(int n = -1) { if (n < 0) scanf("%d", &n); VI a(n); repn(i, n) scanf("%d", &a[i]); return a; }
+VL read_vl(int n = -1) { if (n < 0) scanf("%d", &n); VL a(n); repn(i, n) scanf("%lld", &a[i]); return a; }
+VI read_vpi(int n = -1) { if (n < 0) scanf("%d", &n); VPI a(n); repn(i, n) scanf("%d%d", &a[i].fi,&a[i].se); return a; }
+VI read_vpl(int n = -1) { if (n < 0) scanf("%d", &n); VPL a(n); repn(i, n) scanf("%lld%lld", &a[i].fi,&a[i].se); return a; }
 inline LL powmod(LL a, LL b, LL m) { LL r = 1; for(; b > 0; b >>= 1, a = a * a % m) { if(b & 1) r = r * a % m; } return r; }
 // clang-format on
 // }}}
