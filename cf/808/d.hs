18533c649b02c8634a6d0879ac6e2e64f8728d46
Qiwei Feng <caiwaifung@gmail.com>
2017-05-23 20:59:58 -0700

    .

--- cf/808/d.hs
+++ cf/808/d.hs
@@ -11,3 +11,15 @@ getIntegers = map (fst . fromJust . B.readInteger) <$> B.words <$> B.getContents
 pair []       = []
 pair (x:y:rs) = (x, y) : pair rs
 clamp u v w = max u $ min v $ w
+
+main = do
+    _:xs <- getIntegers
+    putStrLn $ if solve xs then "YES" else "NO"
+
+solve xs = mod s 2 == 0 && (res1 || res2) where
+    s = sum xs
+    want = - div s 2
+    res1 = go xs want S.empty
+    res2 = go (reverse xs) want S.empty
+    go [] want s     = S.member want s
+    go (x:xs) want s = S.member want s || go xs (want+x) (S.insert x s)
