f8cb403a55d67e4eed93f5f7345275929030fbe9
Qiwei Feng <caiwaifung@gmail.com>
2017-05-25 11:33:24 -0700

    .

---
+++ yandex/2015/r1/d.cc
@@ -0,0 +1,114 @@
+// 10:55
+// 11:12 - eta 11:20
+//       - 11:20 - wa sample
+//       - 11:28 - pass
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+// {{{
+// clang-format off
+#define all(a) (a).begin(), (a).end()
+#define sz(a) static_cast<int>((a).size())
+#define fillchar(a, x) memset(a, x, sizeof(a))
+#define rep(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define irep(i, a, b) for (int i = int(a); i >= int(b); --i)
+#define replr(i, a, b) rep(i, a, (b)-1)
+#define reprl(i, a, b) irep(i, (b)-1, a)
+#define repn(i, n) rep(i, 0, (n)-1)
+#define irepn(i, n) irep(i, (n)-1, 0)
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define popcount __builtin_popcount
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<double> VD;
+typedef vector<bool> VB;
+typedef vector<string> VS;
+typedef vector<PII> VPI;
+typedef vector<PLL> VPL;
+typedef vector<PDD> VPD;
+typedef vector<VI> VVI;
+typedef vector<VL> VVL;
+typedef vector<VD> VVD;
+typedef vector<VB> VVB;
+typedef vector<VS> VVS;
+typedef vector<VPI> VVPI;
+typedef vector<VPL> VVPL;
+template <class T, class S> ostream& operator<<(ostream& os, const pair<T, S>& v) { return os << "(" << v.first << ", " << v.second << ")"; }
+template <class T> ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; repn(i, sz(v)) { if(i) os << ", "; os << v[i]; } return os << "]"; }
+template <class T> bool setmax(T& _a, T _b) { if(_a < _b) { _a = _b; return true; } return false; }
+template <class T> bool setmin(T& _a, T _b) { if(_b < _a) { _a = _b; return true; } return false; }
+template <class T> T gcd(T _a, T _b) { return _b == 0 ? _a : gcd(_b, _a % _b); }
+VI read_vi(int n = -1) { if (n < 0) scanf("%d", &n); VI a(n); repn(i, n) scanf("%d", &a[i]); return a; }
+VL read_vl(int n = -1) { if (n < 0) scanf("%d", &n); VL a(n); repn(i, n) scanf("%lld", &a[i]); return a; }
+VD read_vd(int n = -1) { if (n < 0) scanf("%d", &n); VD a(n); repn(i, n) scanf("%lf", &a[i]); return a; }
+VPI read_vpi(int n = -1) { if (n < 0) scanf("%d", &n); VPI a(n); repn(i, n) scanf("%d%d", &a[i].fi,&a[i].se); return a; }
+VPL read_vpl(int n = -1) { if (n < 0) scanf("%d", &n); VPL a(n); repn(i, n) scanf("%lld%lld", &a[i].fi,&a[i].se); return a; }
+VPD read_vpd(int n = -1) { if (n < 0) scanf("%d", &n); VPD a(n); repn(i, n) scanf("%lf%lf", &a[i].fi,&a[i].se); return a; }
+inline LL powmod(LL a, LL b, LL m) { LL r = 1; for(; b > 0; b >>= 1, a = a * a % m) { if(b & 1) r = r * a % m; } return r; }
+// clang-format on
+// }}}
+
+LL solve(LL n0, LL r0) {
+    VI n, r;
+    while(n0 > 0 || r0 > 0) {
+        n.pb(n0 % 2), r.pb(r0 % 2);
+        n0 /= 2, r0 /= 2;
+    }
+    static LL f[66][4][2];
+    fillchar(f, 0);
+    f[0][1][0] = 1;
+    repn(i, sz(n)) rep(s, 1, 3) repn(gt, 2) {
+        VI next_s(2);
+        repn(carry, 2) if(s & (1 << carry)) {
+            repn(bit1, 2) {
+                int bit2 = bit1 + n[i] + carry;
+                int cur = bit1 ^ (bit2 & 1);
+                int next_carry = bit2 / 2;
+                next_s[cur] |= (1 << next_carry);
+            }
+        }
+        // cout<<i<<" "<<s<<" "<<gt<<" "<<next_s<<endl;
+        repn(cur, 2) {
+            int next_gt = gt;
+            if(cur != r[i]) next_gt = (cur > r[i]);
+            f[i + 1][next_s[cur]][next_gt] += f[i][s][gt];
+        }
+    }
+    return f[sz(n)][1][0] + f[sz(n)][3][0];
+}
+
+int main() {
+    LL n, l, r;
+    cin >> n >> l >> r;
+    LL ans = solve(n, r);
+    if(l > 0) ans -= solve(n, l - 1);
+    cout << ans << endl;
+    return 0;
+}
