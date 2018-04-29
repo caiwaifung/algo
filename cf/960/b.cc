62617dec25f802ac4c7ae42a602937e8c6abf57d
Qiwei Feng <caiwaifung@gmail.com>
2018-04-29 16:27:56 +0100

    CF 960

---
+++ cf/960/b.cc
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+// {{{
+// clang-format off
+#define all(a) (a).begin(), (a).end()
+#define len(a) static_cast<int>((a).size())
+#define fillchar(a, x) memset(a, x, sizeof(a))
+#define rep(i, a, b) for (int i = int(a); i <= int(b); ++i)
+#define irep(i, a, b) for (int i = int(a); i >= int(b); --i)
+#define replr(i, a, b) rep(i, a, (b)-1)
+#define reprl(i, a, b) irep(i, (b)-1, a)
+#define repn(i, n) rep(i, 0, (n)-1)
+#define irepn(i, n) irep(i, (n)-1, 0)
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define popcount __builtin_popcount
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+typedef pair<double, double> PDD;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<double> VD;
+typedef vector<bool> VB;
+typedef vector<string> VS;
+typedef vector<PII> VPI;
+typedef vector<PLL> VPL;
+typedef vector<PDD> VPD;
+typedef vector<VI> VVI;
+typedef vector<VL> VVL;
+typedef vector<VD> VVD;
+typedef vector<VB> VVB;
+typedef vector<VS> VVS;
+typedef vector<VPI> VVPI;
+typedef vector<VPL> VVPL;
+template <class T, class S> ostream& operator<<(ostream& os, const pair<T, S>& v) { return os << "(" << v.first << ", " << v.second << ")"; }
+template <class T> ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; repn(i, sz(v)) { if(i) os << ", "; os << v[i]; } return os << "]"; }
+template <class T> bool setmax(T& _a, T _b) { if(_a < _b) { _a = _b; return true; } return false; }
+template <class T> bool setmin(T& _a, T _b) { if(_b < _a) { _a = _b; return true; } return false; }
+template <class T> T gcd(T _a, T _b) { return _b == 0 ? _a : gcd(_b, _a % _b); }
+int read_int() { int x; scanf("%d", &x); return x; }
+LL read_ll() { LL x; scanf("%lld", &x); return x; }
+string read_string() { string s; cin >> s; return s; }
+VI read_vi(int n = -1) { if (n < 0) scanf("%d", &n); VI a(n); repn(i, n) scanf("%d", &a[i]); return a; }
+VL read_vl(int n = -1) { if (n < 0) scanf("%d", &n); VL a(n); repn(i, n) scanf("%lld", &a[i]); return a; }
+VD read_vd(int n = -1) { if (n < 0) scanf("%d", &n); VD a(n); repn(i, n) scanf("%lf", &a[i]); return a; }
+VPI read_vpi(int n = -1) { if (n < 0) scanf("%d", &n); VPI a(n); repn(i, n) scanf("%d%d", &a[i].fi,&a[i].se); return a; }
+VPL read_vpl(int n = -1) { if (n < 0) scanf("%d", &n); VPL a(n); repn(i, n) scanf("%lld%lld", &a[i].fi,&a[i].se); return a; }
+VPD read_vpd(int n = -1) { if (n < 0) scanf("%d", &n); VPD a(n); repn(i, n) scanf("%lf%lf", &a[i].fi,&a[i].se); return a; }
+inline LL powmod(LL a, LL b, LL m) { LL r = 1; for(; b > 0; b >>= 1, a = a * a % m) { if(b & 1) r = r * a % m; } return r; }
+#define IN read_int()
+// clang-format on
+// }}}
+
+int main() {
+    int n = IN, k1 = IN, k2 = IN, k = k1 + k2;
+    VL a = read_vl(n);
+    VL b = read_vl(n);
+
+    const LL inf = 1ll << 60;
+    VL f(k + 1, inf);
+    f[0] = 0;
+    repn(i, n) {
+        const LL u = llabs(a[i] - b[i]);
+        VL f2(k + 1, inf);
+        repn(x, k + 1) {
+            rep(p, 0, k - x) {
+                LL tmp = u - p;
+                if (tmp < 0) tmp &= 1;
+                setmin(f2[x + p], f[x] + tmp * tmp);
+            }
+        }
+        f = f2;
+    }
+    cout << f[k] << endl;
+    return 0;
+}
